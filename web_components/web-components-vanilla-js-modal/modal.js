89c67cc88660161f13bfbe1127d4c5670c254f95
Wajdzik, Maciej <wajdzik.maciej@gmail.com>
2019-10-04 22:07:47 +0200

    Add modal

---
+++ web_components/web-components-vanilla-js-modal/modal.js
@@ -0,0 +1,98 @@
+class Modal extends HTMLElement {
+
+    constructor() {
+        super();
+        this.attachShadow({mode: 'open'});
+        this.shadowRoot.innerHTML = `
+            <style>
+                #backdrop {
+                    position: fixed;
+                    top: 0;
+                    left: 0;
+                    width: 100%;
+                    height: 100vh;
+                    background: rgba(0, 0, 0, 0.75);
+                    z-index: 10;
+                    opacity: 0;
+                    pointer-events: none;
+                }
+                
+                :host([opened]) #backdrop,
+                :host([opened]) #modal {
+                    opacity: 1;
+                    pointer-events: all;
+                }
+
+                #modal {
+                    position: fixed;
+                    top: 15vh;
+                    left: 25%;
+                    width: 50%;
+                    background: white;
+                    border-radius: 3px;
+                    box-shadow: 0 2px 8px rgba(0,0,0,0.25);
+                    display: flex;
+                    flex-direction: column;
+                    justify-content: space-between;
+                    z-index: 100;
+                    opacity: 0;
+                    pointer-events: none;                    
+                }
+                
+                header {
+                    padding: 1rem;
+                }
+                
+                header h1 {
+                    font-size: 1.25rem;
+                }
+                
+                #main {
+                    padding: 1rem;
+                }
+                
+                #actions {
+                    border-top: 1px solid #ccc; 
+                    padding: 1rem;
+                    display: flex;
+                    justify-content: flex-end;      
+                }
+                
+                #actions button {
+                    margin: 0 0.25rem;      
+                }
+            </style>
+            
+            <div id="backdrop"></div>
+            
+            <div id="modal">
+                <header>
+                    <h1>Please Confirm</h1>
+                </header>
+                <section id="main">
+                    <slot></slot>
+                </section>
+                <section id="actions">
+                    <button>Cancel</button>
+                    <button>OK</button>
+                </section>
+            </div>
+        `;
+    }
+
+    attributeChangedCallback() {
+        if (name === 'opened') {
+            console.log('opened attribute changed');
+        }
+    }
+
+    static get observedAttributes() {
+        return ['opened'];
+    }
+
+    open() {
+        this.setAttribute('opened', '');
+    }
+}
+
+customElements.define('uc-modal', Modal);
