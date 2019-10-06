cced317ada3139aab38bc66d66859a9cc38f2c8f
Wajdzik, Maciej <wajdzik.maciej@gmail.com>
2019-10-06 13:25:49 +0200

    Add new module

---
+++ web_components/web-components-stencil-stocks/src/components/side-drawer/stock-price.tsx
@@ -0,0 +1,12 @@
+import {Component} from '@stencil/core';
+
+@Component({
+  tag: 'uc-stock-price',
+  styleUrl: './stock-price.css',
+  shadow: true
+})
+export class StockPrice {
+
+  render() {
+  }
+}
