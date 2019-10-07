b4cfdfcfbb24bcec5fa38c9705530ea40ec7eee2
Wajdzik, Maciej <wajdzik.maciej@gmail.com>
2019-10-07 15:54:03 +0200

    Add a find stocks component

---
+++ web_components/stencil-stocks/src/components/stock-finder/stock-finder.tsx
@@ -0,0 +1,51 @@
+import {Component, State} from '@stencil/core';
+import {AV_API_KEY} from "../../global/api";
+
+@Component({
+  tag: 'uc-stock-finder',
+  styleUrl: './stock-finder.css',
+  shadow: true
+})
+export class StockPrice {
+
+  stockNameInput: HTMLInputElement;
+
+  @State() searchResults: { symbol: string, name: string }[] = [];
+
+  onFindStocks(event: Event) {
+    event.preventDefault();
+
+    const stockSymbol = this.stockNameInput.value;
+    this.searchResults = [];
+
+    fetch(`https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=${stockSymbol}&apikey=${AV_API_KEY}`)
+      .then(res => res.json())
+      .then(res => {
+        this.searchResults = res['bestMatches'].map(match => {
+          return {symbol: match['1. symbol'], name: match['2. name']}
+        });
+      })
+      .catch(err => {
+        console.log(err);
+      })
+  }
+
+  render() {
+    return [
+      <form onSubmit={this.onFindStocks.bind(this)}>
+        <input id="stock-symbol"
+               ref={el => this.stockNameInput = el}/>
+        <button type="submit">
+          Find
+        </button>
+      </form>,
+      <ul>
+        {this.searchResults.map(result => (
+          <li>
+            <strong>{result.symbol}</strong> - {result.name}
+          </li>
+        ))}
+      </ul>
+    ]
+  }
+}
