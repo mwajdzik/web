82ef84e79179938ab96fabac4c0769adbb6dbc98
Wajdzik, Maciej <wajdzik.maciej@gmail.com>
2019-10-14 16:42:31 +0200

    Prepare SVG

--- web_components/stencil-my-components/src/components/bar-chart/bar-chart.tsx
+++ web_components/stencil-my-components/src/components/bar-chart/bar-chart.tsx
@@ -10,9 +10,28 @@ export class BarChart {
   @Element() el: HTMLElement;
 
   render() {
+    let loading = false;
+
+    let captionSection;
+    let svgSection;
+
+    if (loading) {
+      captionSection = (
+        <div class="caption">
+          <p>Loading...</p>
+        </div>
+      );
+    } else {
+      svgSection = (
+        <svg xmlns="http://www.w3.org/2000/svg">
+        </svg>
+      )
+    }
+
     return [
-      <div>
-      </div>
+      <figure>
+        {captionSection || svgSection}
+      </figure>
     ]
   }
 }
